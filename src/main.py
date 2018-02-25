ffc6c6dcd45b63d8ff7fea44fb44f00f63ffc5e6
Quentin Vecchio <vecchioquentin@hotmail.fr>
2018-02-25 13:58:31 +0100

    Add beginning of the lab

---
+++ src/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import scenarios
+
+def run():
+    scenarios.scenario3_1()
+    # scenarios.scenario3_2()
+    # scenarios.scenario3_3()
+    # scenarios.scenario3_4()
+    # scenarios.scenario3_5()
+    # scenarios.scenario3_6()
+
+if __name__ == '__main__':
+    run()
---
+++ src/scenarios.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+def scenario3_1():
+    print("TODO")
+
+def scenario3_2():
+    print("TODO")
+
+def scenario3_3():
+    print("TODO")
+
+def scenario3_4():
+    print("TODO")
+
+def scenario3_5():
+    print("TODO")
+
+def scenario3_6():
+    print("TODO")
